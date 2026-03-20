c43d01777f6ff07206f475a8189f86b94f92ee73
Jacqueline <terijacquie@gmail.com>
2026-03-20 11:45:32 +0000

    Metodo para listar tareas

--- src/GestorTareas.java
+++ src/GestorTareas.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class GestorTareas {
+
+        ArrayList<Tarea> tareas;
+
+    public GestorTareas() {
+            this.tareas = new ArrayList<Tarea>();
+        }
+
+
 }
