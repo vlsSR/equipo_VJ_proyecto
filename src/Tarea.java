76128cf9e7a0b2d8b6b1294f0fc42f5ebc03d12a
vls <lermasanchezvictor@gmail.com>
2026-03-20 11:10:42 +0000

    Añadir el constructor

--- src/Tarea.java
+++ src/Tarea.java
@@ -2,4 +2,12 @@ public class Tarea {
     private int id;
     private String descripcion;
     private boolean completada;
+
+    public Tarea(int id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+
 }
