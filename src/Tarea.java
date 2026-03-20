071cf7a8254fea03388b6a43cf86fdf52642732c
vls <lermasanchezvictor@gmail.com>
2026-03-20 11:11:21 +0000

    Añadir metodos y terminar clase

--- src/Tarea.java
+++ src/Tarea.java
@@ -9,5 +9,36 @@ public class Tarea {
         this.completada = false;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "id=" + id +
+                ", descripcion='" + descripcion + '\'' +
+                ", completada=" + completada +
+                '}';
+    }
 }
